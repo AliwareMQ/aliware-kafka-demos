ab06171f95aaba246e15eea2e5aaaaf8196d6fe9
章礼平 <LipperZack@gmail.com>
2017-08-15 19:43:27 +0800

    = extract setting.js

---
+++ kafka-nodejs-demo/setting.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'sasl_plain_username': 'xxx',
+    'sasl_plain_password': 'xxx',
+    'bootstrap_servers': ["kafka-ons-internet.aliyun.com:8080"],
+    'topic_name': 'SAINT_TEST',
+    'consumer_id': 'CID-SAINT-TEST',
+    'producer_id': ''
+}
