9a834433e20abc25f8f187c90261f6a6083b6998
zander <zhendongliu.lzd@alibaba-inc.com>
2018-01-26 15:01:13 +0800

    Polish the spring demo

--- kafka-spring-demo/src/main/java/com/aliyun/openservices/kafka/ons/KafkaProducerDemo.java
+++ kafka-spring-demo/src/main/java/com/aliyun/openservices/kafka/ons/KafkaProducerDemo.java
@@ -3,12 +3,19 @@ package com.aliyun.openservices.kafka.ons;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 
 public class KafkaProducerDemo {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws Exception {
         ApplicationContext context = new  ClassPathXmlApplicationContext("kafka-producer.xml");
         KafkaTemplate kafkaTemplate = context.getBean("KafkaTemplate", KafkaTemplate.class);
-        kafkaTemplate.sendDefault(System.currentTimeMillis() + "","hello world");
+        //不指定Topic发送，会把消息发往默认配置的Topic
+        ListenableFuture<SendResult> future = kafkaTemplate.sendDefault(System.currentTimeMillis() + "","hello world");
+        //如果需要同步获得结果，记得调用get
+        future.get();
+        //也可以直接指定Topic进行发送
+        //ListenableFuture<SendResult> future = kafkaTemplate.send("Topic"， System.currentTimeMillis() + "","hello world");
     }
 }
