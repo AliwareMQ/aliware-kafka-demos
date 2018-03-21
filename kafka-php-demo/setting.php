60d6ea8dbc2211cf52d0959ac214570d2255ff1e
zander <zhendongliu.lzd@alibaba-inc.com>
2018-03-21 14:33:32 +0800

    Remove PHP

--- kafka-php-demo/setting.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'sasl_plain_username' => 'xxx',
-    'sasl_plain_password' => 'xxx',
-    'bootstrap_servers' => "kafka-ons-internet.aliyun.com:8080",
-    'topic_name' => 'SAINT_TEST',
-    'consumer_id' => 'CID-SAINT-TEST'
-];
