619c8c08937022cfd4d08ea1cedb2a8917ba6de8
zander <zhendongliu.lzd@alibaba-inc.com>
2018-03-21 14:35:54 +0800

    Polish Java Demo

--- kafka-java-demo/src/main/java/com/aliyun/openservices/kafka/ons/KafkaProducerDemo.java
+++ kafka-java-demo/src/main/java/com/aliyun/openservices/kafka/ons/KafkaProducerDemo.java
@@ -13,40 +13,51 @@ import org.apache.kafka.common.config.SslConfigs;
 public class KafkaProducerDemo {
 
     public static void main(String args[]) {
-        if (System.getProperty("java.security.auth.login.config") == null) {
-            System.setProperty("java.security.auth.login.config",  KafkaProducerDemo.class.getClassLoader().getResource("kafka_client_jaas.conf").getPath());
-        }
+        //设置sasl文件的路径
+        JavaKafkaConfigurer.configureSasl();
+
+        //加载kafka.properties
+        Properties kafkaProperties =  JavaKafkaConfigurer.getKafkaProperties();
 
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "SASL_SSL://kafka-ons-internet.aliyun.com:8080");
-        props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, KafkaProducerDemo.class.getClassLoader().getResource("kafka.client.truststore.jks").getPath());
+        //设置接入点，请通过控制台获取对应Topic的接入点
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getProperty("bootstrap.servers"));
+        //设置SSL根证书的路径，请记得将XXX修改为自己的路径
+        //与sasl路径类似，该文件也不能被打包到jar中
+        props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, kafkaProperties.getProperty("ssl.truststore.location"));
+        //根证书store的密码，保持不变
         props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "KafkaOnsClient");
+        //接入协议，目前支持使用SASL_SSL协议接入
         props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+        //SASL鉴权方式，保持不变
         props.put(SaslConfigs.SASL_MECHANISM, "ONS");
-        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        //Kafka消息的序列化方式
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        //请求的最长等待时间
         props.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 30 * 1000);
+
+        //构造Producer对象，注意，该对象是线程安全的，一般来说，一个进程内一个Producer对象即可；
+        //如果想提高性能，可以多构造几个对象，但不要太多，最好不要超过5个
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
 
-        String message = "This is a message from kafka a";
+        //构造一个Kafka消息
+        String topic = kafkaProperties.getProperty("topic"); //消息所属的Topic，请在控制台申请之后，填写在这里
+        String value = "this is the message's value"; //消息的内容
 
-        String  topic = System.getProperty("kafka.ons.TOPIC", "kafka-ons-test-1");
+        ProducerRecord<String, String>  kafkaMessage =  new ProducerRecord<String, String>(topic, value);
 
-        Future<RecordMetadata> metadataFuture = producer.send(new ProducerRecord<String, String>(
-            topic, //消息所属的Topic
-            null, //消息所属的分区，建议为null，交由系统自动分配
-            System.currentTimeMillis(),//消息的生产时间，如果为null，则由系统取当前时间
-            String.valueOf(message.hashCode()), //消息的Key，可以为null
-            message));   //消息的value，建议不要为null
         try {
+            //发送消息，并获得一个Future对象
+            Future<RecordMetadata> metadataFuture = producer.send(kafkaMessage);
+            //同步获得Future对象的结果
             RecordMetadata recordMetadata = metadataFuture.get();
-            System.out.println("produce ok:" + recordMetadata.toString());
+            System.out.println("Produce ok:" + recordMetadata.toString());
         } catch (Exception e) {
             System.out.println("error occurred");
             e.printStackTrace();
         }
-        producer.flush();
-
     }
+
+
 }
