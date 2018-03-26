48e8a288fddcfc9f8a1070572f6ee6d43eae3f1f
zander <zhendongliu.lzd@alibaba-inc.com>
2018-03-26 22:29:34 +0800

    Add consumer & producer

---
+++ kafka-java-demo/run_consumer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mvn clean package -Dmaven.test.skip=true
+case $1 in 
+    *)
+        echo "run kafka one demo"
+        cd target/KafkaOnsDemo/KafkaOnsDemo/ && java -cp .:./conf/:./lib/* "$@" com.aliyun.openservices.kafka.ons.KafkaConsumerDemo 
+        ;;
+esac
