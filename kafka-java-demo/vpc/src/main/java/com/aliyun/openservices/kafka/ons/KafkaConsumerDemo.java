1f0e62400f9eee6237583dd70198889f31f31dcd
ymwneu <ymwneu@126.com>
2019-07-17 18:19:43 +0800

    add multi thread demo

--- kafka-java-demo/vpc/src/main/java/com/aliyun/openservices/kafka/ons/KafkaConsumerDemo.java
+++ kafka-java-demo/vpc/src/main/java/com/aliyun/openservices/kafka/ons/KafkaConsumerDemo.java
@@ -3,19 +3,18 @@ package com.aliyun.openservices.kafka.ons;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.errors.WakeupException;
 
 public class KafkaConsumerDemo {
 
     public void doConsumer() {
         //加载kafka.properties
-        Properties kafkaProperties = JavaKafkaConfigurer.getKafkaProperties();
+        Properties kafkaProperties =  JavaKafkaConfigurer.getKafkaProperties();
 
         Properties props = new Properties();
         //设置接入点，请通过控制台获取对应Topic的接入点
@@ -32,64 +31,34 @@ public class KafkaConsumerDemo {
         //当前消费实例所属的消费组，请在控制台申请之后填写
         //属于同一个组的消费实例，会负载消费消息
         props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaProperties.getProperty("group.id"));
+        //构造消息对象，也即生成一个消费实例
+        KafkaConsumer<String, String> consumer = new org.apache.kafka.clients.consumer.KafkaConsumer<String, String>(props);
+        //设置消费组订阅的Topic，可以订阅多个
+        //如果GROUP_ID_CONFIG是一样，则订阅的Topic也建议设置成一样
+        List<String> subscribedTopics =  new ArrayList<String>();
+        //如果需要订阅多个Topic，则在这里add进去即可
+        //每个Topic需要先在控制台进行创建
+        subscribedTopics.add(kafkaProperties.getProperty("topic"));
+        consumer.subscribe(subscribedTopics);
 
-        int consumerNum = 2;
-        for (int i = 0; i < consumerNum; i++) {
-            KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
-
-            List<String> subscribedTopics = new ArrayList<String>();
-            subscribedTopics.add(kafkaProperties.getProperty("topic"));
-            consumer.subscribe(subscribedTopics);
-
-            KafkaConsumerRunner kafkaConsumerRunner = new KafkaConsumerRunner(consumer);
-            Thread consumerThread = new Thread(kafkaConsumerRunner);
-            consumerThread.start();
-        }
-    }
-
-    static class KafkaConsumerRunner implements Runnable {
-        private final AtomicBoolean closed = new AtomicBoolean(false);
-        private final KafkaConsumer consumer;
-
-        KafkaConsumerRunner(KafkaConsumer consumer) {
-            this.consumer = consumer;
-        }
-
-        @Override
-        public void run() {
+        //循环消费消息
+        while (true){
             try {
-                while (!closed.get()) {
-                    try {
-                        ConsumerRecords<String, String> records = consumer.poll(1000);
-                        //必须在下次poll之前消费完这些数据, 且总耗时不得超过SESSION_TIMEOUT_MS_CONFIG
-                        for (ConsumerRecord<String, String> record : records) {
-                            System.out.println(String.format("Thread:%s Consume partition:%d offset:%d", Thread.currentThread().getName(), record.partition(), record.offset()));
-                        }
-                    } catch (Exception e) {
-                        try {
-                            Thread.sleep(1000);
-                        } catch (Throwable ignore) {
+                ConsumerRecords<String, String> records = consumer.poll(1000);
+                //必须在下次poll之前消费完这些数据, 且总耗时不得超过SESSION_TIMEOUT_MS_CONFIG
+                //建议开一个单独的线程池来消费消息，然后异步返回结果
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println(String.format("Consume partition:%d offset:%d", record.partition(), record.offset()));
+                }
+            } catch (Exception e) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Throwable ignore) {
 
-                        }
-                        //参考常见报错: https://help.aliyun.com/document_detail/68168.html?spm=a2c4g.11186623.6.567.2OMgCB
-                        e.printStackTrace();
-                    }
                 }
-            } catch (WakeupException e) {
-                // Ignore exception if closing
-                if (!closed.get()) {
-                    throw e;
-                }
-            } finally {
-                consumer.close();
+                //参考常见报错: https://help.aliyun.com/document_detail/68168.html?spm=a2c4g.11186623.6.567.2OMgCB
+                e.printStackTrace();
             }
-        }
-
-        // Shutdown hook which can be called from a separate thread
-        public void shutdown() {
-            closed.set(true);
-            consumer.wakeup();
         }
     }
 }
-
