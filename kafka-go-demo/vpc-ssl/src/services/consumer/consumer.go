7927e1993b3e70238a36915c4c714de00d60c158
zander <zhendongliu.lzd@alibaba-inc.com>
2019-09-18 14:34:56 +0800

    Fix the go clinet version to 0_10_2_2 for the 0_10_2_1 dose not exist

--- kafka-go-demo/vpc-ssl/src/services/consumer/consumer.go
+++ kafka-go-demo/vpc-ssl/src/services/consumer/consumer.go
@@ -51,7 +51,7 @@ func init() {
 	clusterCfg.Consumer.Offsets.Initial = sarama.OffsetOldest
 	clusterCfg.Group.Return.Notifications = true
 
-	clusterCfg.Version = sarama.V0_10_2_2
+	clusterCfg.Version = sarama.V0_10_2_1
 	if err = clusterCfg.Validate(); err != nil {
 		msg := fmt.Sprintf("Kafka consumer config invalidate. config: %v. err: %v", *clusterCfg, err)
 		fmt.Println(msg)
