b9a599f06d330bb4a7df6e4211422ff5f536a355
zander <zhendongliu.lzd@alibaba-inc.com>
2020-03-05 17:34:47 +0800

    Polish kafka python argument

--- kafka-python-demo/vpc-ssl/aliyun_kafka_consumer.py
+++ kafka-python-demo/vpc-ssl/aliyun_kafka_consumer.py
@@ -24,10 +24,13 @@ context.load_verify_locations("ca-cert")
 
 consumer = KafkaConsumer(bootstrap_servers=conf['bootstrap_servers'],
                         group_id=conf['consumer_id'],
+                        api_version = (0,10,2),
+                        session_timeout_ms=25000,
+                        max_poll_records=100,
+                        fetch_max_bytes=1 * 1024 * 1024
+                        security_protocol='SASL_SSL',
                         sasl_mechanism="PLAIN",
                         ssl_context=context,
-                        security_protocol='SASL_SSL',
-                        api_version = (0,10),
                         sasl_plain_username=conf['sasl_plain_username'],
                         sasl_plain_password=conf['sasl_plain_password'])
 
--- kafka-python-demo/vpc/aliyun_kafka_consumer.py
+++ kafka-python-demo/vpc/aliyun_kafka_consumer.py
@@ -10,7 +10,10 @@ conf = setting.kafka_setting
 
 consumer = KafkaConsumer(bootstrap_servers=conf['bootstrap_servers'],
                         group_id=conf['consumer_id'],
-                        api_version = (0,10))
+                        api_version = (0,10,2), 
+                        session_timeout_ms=25000,
+                        max_poll_records=100,
+                        fetch_max_bytes=1 * 1024 * 1024)
 
 print 'consumer start to consuming...'
 consumer.subscribe((conf['topic_name'], ))
