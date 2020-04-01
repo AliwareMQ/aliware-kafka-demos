c8c6e42c9a4f90119f40b79f7aee877e8b2386db
zander <zhendongliu.lzd@alibaba-inc.com>
2020-04-01 17:03:26 +0800

    Fix python demo

--- kafka-python-demo/vpc-ssl/aliyun_kafka_consumer.py
+++ kafka-python-demo/vpc-ssl/aliyun_kafka_consumer.py
@@ -27,7 +27,7 @@ consumer = KafkaConsumer(bootstrap_servers=conf['bootstrap_servers'],
                         api_version = (0,10,2),
                         session_timeout_ms=25000,
                         max_poll_records=100,
-                        fetch_max_bytes=1 * 1024 * 1024
+                        fetch_max_bytes=1 * 1024 * 1024,
                         security_protocol='SASL_SSL',
                         sasl_mechanism="PLAIN",
                         ssl_context=context,
