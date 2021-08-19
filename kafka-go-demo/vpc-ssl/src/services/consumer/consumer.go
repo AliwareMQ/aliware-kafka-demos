0fcb8e32b4745ef037c2a74f1200ca5389a762ad
zander <zhendongliu.lzd@alibaba-inc.com>
2021-08-19 11:28:25 +0800

    Use new producer

--- kafka-go-demo/vpc-ssl/src/services/consumer/consumer.go
+++ kafka-go-demo/vpc-ssl/src/services/consumer/consumer.go
@@ -1,120 +1,186 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"crypto/x509"
-	"fmt"
-	"io/ioutil"
-	configs "kafkasaramagodemo/src/services"
-	"os"
-	"time"
-
-	"os/signal"
+	"encoding/json"
 	"github.com/Shopify/sarama"
-	"github.com/bsm/sarama-cluster"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strings"
+	"sync"
+	"syscall"
 )
 
-var cfg *configs.MqConfig
-var consumer *cluster.Consumer
-var sig chan os.Signal
 
-func init() {
-	fmt.Println("init kafka consumer, it may take a few seconds...")
+type KafkaConfig struct {
+	Topic      string `json:"topic"`
+	Topic2      string `json:"topic2"`
+	GroupId    string `json:"group.id"`
+	BootstrapServers    string `json:"bootstrap.servers"`
+	SecurityProtocol string `json:"security.protocol"`
+	SaslMechanism string `json:"sasl.mechanism"`
+	SaslUsername string `json:"sasl.username"`
+	SaslPassword string `json:"sasl.password"`
+}
 
-	var err error
 
-	cfg := &configs.MqConfig{}
-	configs.LoadJsonConfig(cfg, "kafka.json")
-
-	clusterCfg := cluster.NewConfig()
-
-	clusterCfg.Net.SASL.Enable = true
-	clusterCfg.Net.SASL.User = cfg.Ak
-	clusterCfg.Net.SASL.Password = cfg.Password
-	clusterCfg.Net.SASL.Handshake = true
-
-	certBytes, err := ioutil.ReadFile(configs.GetFullPath(cfg.CertFile))
-	clientCertPool := x509.NewCertPool()
-	ok := clientCertPool.AppendCertsFromPEM(certBytes)
-	if !ok {
-		panic("kafka consumer failed to parse root certificate")
-	}
-
-	clusterCfg.Net.TLS.Config = &tls.Config{
-		//Certificates:       []tls.Certificate{},
-		RootCAs:            clientCertPool,
-		InsecureSkipVerify: true,
-	}
-
-	clusterCfg.Net.TLS.Enable = true
-	clusterCfg.Consumer.Return.Errors = true
-	clusterCfg.Consumer.Offsets.CommitInterval =  1 * time.Second
-	clusterCfg.Consumer.Offsets.Initial = sarama.OffsetNewest
-	clusterCfg.Group.Return.Notifications = true
-
-	clusterCfg.Version = sarama.V0_10_2_1
-	if err = clusterCfg.Validate(); err != nil {
-		msg := fmt.Sprintf("Kafka consumer config invalidate. config: %v. err: %v", *clusterCfg, err)
-		fmt.Println(msg)
-		panic(msg)
-	}
-
-	consumer, err = cluster.NewConsumer(cfg.Servers, cfg.ConsumerId, cfg.Topics, clusterCfg)
+func getFullPath(file string) string {
+	workPath, err := os.Getwd()
 	if err != nil {
-		msg := fmt.Sprintf("Create kafka consumer error: %v. config: %v", err, clusterCfg)
-		fmt.Println(msg)
-		panic(msg)
+		panic(err)
+	}
+	configPath := filepath.Join(workPath, "conf")
+	fullPath := filepath.Join(configPath, file)
+	return fullPath
+}
+
+
+// config should be a pointer to structure, if not, panic
+func loadJsonConfig() *KafkaConfig {
+	fullPath := getFullPath("kafka.json")
+	file, err := os.Open(fullPath)
+	if err != nil {
+		log.Panicf("Can not load config at %s. Error: %v", fullPath, err)
 	}
 
-	sig = make(chan os.Signal, 1)
+	defer file.Close()
 
-}
-
-func Start() {
-	go consume()
-}
-
-func consume() {
-	for {
-		select {
-		case msg, more := <-consumer.Messages():
-			if more {
-                fmt.Printf("Partition:%d, Offset:%d, Key:%s, Value:%s, Timestamp:%s\n", msg.Partition, msg.Offset, string(msg.Key), string(msg.Value), msg.Timestamp)
-				consumer.MarkOffset(msg, "") // mark message as processed
-			}
-		case err, more := <-consumer.Errors():
-			if more {
-				fmt.Println("Kafka consumer error: %v", err.Error())
-			}
-		case ntf, more := <-consumer.Notifications():
-			if more {
-				fmt.Println("Kafka consumer rebalance: %v", ntf)
-			}
-		case <-sig:
-			fmt.Errorf("Stop consumer server...")
-			consumer.Close()
-			return
-		}
+	decoder := json.NewDecoder(file)
+	var config = &KafkaConfig{}
+	err = decoder.Decode(config)
+	if err != nil {
+		log.Panicf("Decode json fail for config file at %s. Error: %v", fullPath, err)
 	}
-
-}
-
-func Stop(s os.Signal) {
-	fmt.Println("Recived kafka consumer stop signal...")
-	sig <- s
-	fmt.Println("kafka consumer stopped!!!")
+	json.Marshal(config)
+	return  config
 }
 
 func main() {
+	log.Println("Starting a new Sarama consumer")
 
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
+	cfg := loadJsonConfig()
+	mqConfig := sarama.NewConfig()
+	mqConfig.Version = sarama.V2_2_0_0
+	// set to newest in production environment to avoid large amount of duplication
+	mqConfig.Consumer.Offsets.Initial = sarama.OffsetNewest
 
-	Start()
+	switch cfg.SecurityProtocol {
+		case "plaintext" :
+			//do nothing
+		case "sasl_ssl":
+			mqConfig.Net.SASL.Enable = true
+			mqConfig.Net.SASL.User = cfg.SaslUsername
+			mqConfig.Net.SASL.Password = cfg.SaslPassword
+			mqConfig.Net.SASL.Handshake = true
 
-	select {
-	case s := <-signals:
-		Stop(s)
+			certBytes, err := ioutil.ReadFile(getFullPath("ca-cert"))
+			if err != nil {
+				log.Panicf("kafka client read cert file failed %v", err)
+			}
+			clientCertPool := x509.NewCertPool()
+			ok := clientCertPool.AppendCertsFromPEM(certBytes)
+			if !ok {
+				log.Panicf("kafka client failed to parse root certificate")
+			}
+			mqConfig.Net.TLS.Config = &tls.Config{
+				RootCAs:            clientCertPool,
+				InsecureSkipVerify: true,
+			}
+			mqConfig.Net.TLS.Enable = true
+		case "sasl_plaintext":
+			mqConfig.Net.SASL.Enable = true
+			mqConfig.Net.SASL.User = cfg.SaslUsername
+			mqConfig.Net.SASL.Password = cfg.SaslPassword
+			mqConfig.Net.SASL.Handshake = true
+		default:
+			log.Panicf("unknown protocol %v", cfg.SecurityProtocol)
 	}
 
+	if err := mqConfig.Validate(); err != nil {
+		log.Panicf("Kafka producer config invalidate. config: %v. err: %v", *cfg, err)
+	}
+
+
+	/**
+	 * Setup a new Sarama consumer group
+	 */
+	consumer := Consumer{
+		ready: make(chan bool),
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	client, err := sarama.NewConsumerGroup(strings.Split(cfg.BootstrapServers, ","), cfg.GroupId, mqConfig)
+	if err != nil {
+		log.Panicf("Error creating consumer group client: %v", err)
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			if err := client.Consume(ctx, []string{cfg.Topic, cfg.Topic2}, &consumer); err != nil {
+				log.Panicf("Error from consumer: %v", err)
+			}
+			// check if context was cancelled, signaling that the consumer should stop
+			if ctx.Err() != nil {
+				return
+			}
+			consumer.ready = make(chan bool)
+		}
+	}()
+
+	<-consumer.ready // Await till the consumer has been set up
+	log.Println("Sarama consumer up and running!...")
+
+	sigterm := make(chan os.Signal, 1)
+	signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case <-ctx.Done():
+		log.Println("terminating: context cancelled")
+	case <-sigterm:
+		log.Println("terminating: via signal")
+	}
+	cancel()
+	wg.Wait()
+	if err = client.Close(); err != nil {
+		log.Panicf("Error closing client: %v", err)
+	}
 }
+
+// Consumer represents a Sarama consumer group consumer
+type Consumer struct {
+	ready chan bool
+}
+
+// Setup is run at the beginning of a new session, before ConsumeClaim
+func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
+	// Mark the consumer as ready
+	close(consumer.ready)
+	return nil
+}
+
+// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
+func (consumer *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
+func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+
+	// NOTE:
+	// Do not move the code below to a goroutine.
+	// The `ConsumeClaim` itself is called within a goroutine, see:
+	// https://github.com/Shopify/sarama/blob/master/consumer_group.go#L27-L29
+	for message := range claim.Messages() {
+		log.Printf("Message claimed: value = %s, offset = %v, topic = %s, partition =%v", string(message.Value), message.Offset, message.Topic, message.Partition)
+		session.MarkMessage(message, "")
+	}
+	return nil
+}
+
--- kafka-go-demo/vpc-ssl/src/services/producer/producer.go
+++ kafka-go-demo/vpc-ssl/src/services/producer/producer.go
@@ -3,69 +3,63 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	configs "kafkasaramagodemo/src/services"
-    "time"
-    "strconv"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/Shopify/sarama"
 )
 
-
-
-var cfg *configs.MqConfig
-var producer sarama.SyncProducer
-
-func init() {
-
-	fmt.Print("init kafka producer, it may take a few seconds to init the connection\n")
-
-	var err error
-
-	cfg = &configs.MqConfig{}
-	configs.LoadJsonConfig(cfg, "kafka.json")
-
-	mqConfig := sarama.NewConfig()
-	mqConfig.Net.SASL.Enable = true
-	mqConfig.Net.SASL.User = cfg.Ak
-	mqConfig.Net.SASL.Password = cfg.Password
-	mqConfig.Net.SASL.Handshake = true
-
-    mqConfig.Version=sarama.V0_10_2_1
-
-	certBytes, err := ioutil.ReadFile(configs.GetFullPath(cfg.CertFile))
-	clientCertPool := x509.NewCertPool()
-	ok := clientCertPool.AppendCertsFromPEM(certBytes)
-	if !ok {
-		panic("kafka producer failed to parse root certificate")
-	}
-
-	mqConfig.Net.TLS.Config = &tls.Config{
-		//Certificates:       []tls.Certificate{},
-		RootCAs:            clientCertPool,
-		InsecureSkipVerify: true,
-	}
-
-	mqConfig.Net.TLS.Enable = true
-	mqConfig.Producer.Return.Successes = true
-
-	if err = mqConfig.Validate(); err != nil {
-		msg := fmt.Sprintf("Kafka producer config invalidate. config: %v. err: %v", *cfg, err)
-		fmt.Println(msg)
-		panic(msg)
-	}
-
-	producer, err = sarama.NewSyncProducer(cfg.Servers, mqConfig)
-	if err != nil {
-		msg := fmt.Sprintf("Kafak producer create fail. err: %v", err)
-		fmt.Println(msg)
-		panic(msg)
-	}
-
+type KafkaConfig struct {
+	Topic      string `json:"topic"`
+	Topic2      string `json:"topic2"`
+	GroupId    string `json:"group.id"`
+	BootstrapServers    string `json:"bootstrap.servers"`
+	SecurityProtocol string `json:"security.protocol"`
+	SaslMechanism string `json:"sasl.mechanism"`
+	SaslUsername string `json:"sasl.username"`
+	SaslPassword string `json:"sasl.password"`
 }
 
-func produce(topic string, key string, content string) error {
+
+func getFullPath(file string) string {
+	workPath, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	configPath := filepath.Join(workPath, "conf")
+	fullPath := filepath.Join(configPath, file)
+	return fullPath
+}
+
+
+// config should be a pointer to structure, if not, panic
+func loadJsonConfig() *KafkaConfig {
+	fullPath := getFullPath("kafka.json")
+	file, err := os.Open(fullPath)
+	if err != nil {
+		log.Panicf("Can not load config at %s. Error: %v", fullPath, err)
+	}
+
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	var config = &KafkaConfig{}
+	err = decoder.Decode(config)
+	if err != nil {
+		log.Panicf("Decode json fail for config file at %s. Error: %v", fullPath, err)
+	}
+	json.Marshal(config)
+	return  config
+}
+
+func produce(producer sarama.SyncProducer, topic string, key string, content string) error {
 	msg := &sarama.ProducerMessage{
 		Topic: topic,
 		Key:   sarama.StringEncoder(key),
@@ -85,14 +79,69 @@ func produce(topic string, key string, content string) error {
 }
 
 func main() {
-    //the key of the kafka messages 
+
+	log.Println("Starting a new Sarama consumer")
+
+	cfg := loadJsonConfig()
+	mqConfig := sarama.NewConfig()
+	mqConfig.Version = sarama.V2_2_0_0
+	mqConfig.Producer.Return.Successes = true
+	mqConfig.Producer.Partitioner = NewStickyPartitioner
+
+
+	switch cfg.SecurityProtocol {
+	case "plaintext" :
+		//do nothing
+	case "sasl_ssl":
+		mqConfig.Net.SASL.Enable = true
+		mqConfig.Net.SASL.User = cfg.SaslUsername
+		mqConfig.Net.SASL.Password = cfg.SaslPassword
+		mqConfig.Net.SASL.Handshake = true
+
+		certBytes, err := ioutil.ReadFile(getFullPath("ca-cert"))
+		if err != nil {
+			log.Panicf("kafka client read cert file failed %v", err)
+		}
+		clientCertPool := x509.NewCertPool()
+		ok := clientCertPool.AppendCertsFromPEM(certBytes)
+		if !ok {
+			log.Panicf("kafka client failed to parse root certificate")
+		}
+		mqConfig.Net.TLS.Config = &tls.Config{
+			RootCAs:            clientCertPool,
+			InsecureSkipVerify: true,
+		}
+		mqConfig.Net.TLS.Enable = true
+	case "sasl_plaintext":
+		mqConfig.Net.SASL.Enable = true
+		mqConfig.Net.SASL.User = cfg.SaslUsername
+		mqConfig.Net.SASL.Password = cfg.SaslPassword
+		mqConfig.Net.SASL.Handshake = true
+	default:
+		log.Panicf("unknown protocol %v", cfg.SecurityProtocol)
+	}
+
+	if err := mqConfig.Validate(); err != nil {
+		log.Panicf("Kafka producer config invalidate. config: %v. err: %v", *cfg, err)
+	}
+
+	producer, err := sarama.NewSyncProducer(strings.Split(cfg.BootstrapServers, ","), mqConfig)
+	if err != nil {
+		log.Panicf("Kafak producer create fail. err: %v", err)
+	}
+
+	//the key of the kafka messages
     //do not set the same the key for all messages, it may cause partition im-balance
 	i := 0
 	for {
 		key := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
 		i = i + 1
 		value := "this is a kafka message from sarama go " + strconv.Itoa(i)
-		produce(cfg.Topics[0], key, value)
+		if i % 2 == 0 {
+			produce(producer, cfg.Topic2, key, value)
+		} else {
+			produce(producer, cfg.Topic, key, value)
+		}
 		time.Sleep(time.Duration(1) * time.Millisecond)
 	}
 }
