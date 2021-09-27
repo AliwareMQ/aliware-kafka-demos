471b09cd4b14a96aaf6792c33438a72256f60314
ljlj01167294 <lj01167294@alibaba-inc.com>
2021-09-27 17:01:02 +0800

    update kafka-python-demo/vpc-ssl/aliyun_kafka_consumer.py.

--- kafka-python-demo/vpc-ssl/aliyun_kafka_consumer.py
+++ kafka-python-demo/vpc-ssl/aliyun_kafka_consumer.py
@@ -24,7 +24,7 @@ context.load_verify_locations("ca-cert")
 
 consumer = KafkaConsumer(bootstrap_servers=conf['bootstrap_servers'],
                         group_id=conf['consumer_id'],
-                        api_version = (0,10,2),
+                        api_version = (0,10),
                         session_timeout_ms=25000,
                         max_poll_records=100,
                         fetch_max_bytes=1 * 1024 * 1024,
