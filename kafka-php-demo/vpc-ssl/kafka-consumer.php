324b193d46757a72e49d8a470df9660f53fba4c6
lj01167294 <lj01167294@alibaba-inc.com>
2021-11-04 15:38:40 +0800

    php demo update

--- kafka-php-demo/vpc-ssl/kafka-consumer.php
+++ kafka-php-demo/vpc-ssl/kafka-consumer.php
@@ -1,16 +1,27 @@
 <?php
 $setting = require __DIR__ . '/setting.php';
 $conf = new RdKafka\Conf();
+// SASL 验证机制类型默认选用 PLAIN
 $conf->set('sasl.mechanisms', 'PLAIN');
 $conf->set('api.version.request', 'true');
+// 设置用户名：控制台 配置信息 的用户名
 $conf->set('sasl.username', $setting['sasl_plain_username']);
+// 设置密码：控制台 配置信息 的密码
 $conf->set('sasl.password', $setting['sasl_plain_password']);
 $conf->set('security.protocol', 'SASL_SSL');
 $conf->set('ssl.ca.location', __DIR__ . '/ca-cert.pem');
 
 $conf->set('group.id', $setting['consumer_id']);
-
+// 设置入口服务，请通过控制台获取对应的服务地址。
 $conf->set('metadata.broker.list', $setting['bootstrap_servers']);
+
+// 使用 Kafka 消费分组机制时，消费者超时时间。当 Broker 在该时间内没有收到消费者的心跳时，
+// 认为该消费者故障失败，Broker 发起重新 Rebalance 过程。
+$conf->set('session.timeout.ms', 10000);
+// 客户端请求超时时间，如果超过这个时间没有收到应答，则请求超时失败
+$conf->set('request.timeout.ms', 305000);
+// 设置客户端内部重试间隔。
+$conf->set('reconnect.backoff.max.ms', 3000);
 
 $topicConf = new RdKafka\TopicConf();
 
@@ -27,7 +38,7 @@ while (true) {
     $message = $consumer->consume(30 * 1000);
     switch ($message->err) {
         case RD_KAFKA_RESP_ERR_NO_ERROR:
-            var_dump($message);
+            echo "[Consumer] receives the message：" . var_export($message, true) . "\n";
             break;
         case RD_KAFKA_RESP_ERR__PARTITION_EOF:
             echo "No more messages; will wait for more\n";
--- kafka-php-demo/vpc/kafka-consumer.php
+++ kafka-php-demo/vpc/kafka-consumer.php
@@ -4,8 +4,15 @@ $conf = new RdKafka\Conf();
 $conf->set('api.version.request', 'true');
 
 $conf->set('group.id', $setting['consumer_id']);
-
+// 设置入口服务，请通过控制台获取对应的服务地址。
 $conf->set('metadata.broker.list', $setting['bootstrap_servers']);
+// 使用 Kafka 消费分组机制时，消费者超时时间。当 Broker 在该时间内没有收到消费者的心跳时，
+// 认为该消费者故障失败，Broker 发起重新 Rebalance 过程。
+$conf->set('session.timeout.ms', 10000);
+// 客户端请求超时时间，如果超过这个时间没有收到应答，则请求超时失败
+$conf->set('request.timeout.ms', 305000);
+// 设置客户端内部重试间隔。
+$conf->set('reconnect.backoff.max.ms', 3000);
 
 $topicConf = new RdKafka\TopicConf();
 
@@ -13,16 +20,18 @@ $conf->setDefaultTopicConf($topicConf);
 
 $consumer = new RdKafka\KafkaConsumer($conf);
 
+// Debug 时请设置为 LOG_DEBUG
+//$consumer->setLogLevel(LOG_DEBUG);
 $consumer->subscribe([$setting['topic_name']]);
 
 echo "Waiting for partition assignment... (make take some time when\n";
 echo "quickly re-joining the group after leaving it.)\n";
 
 while (true) {
-    $message = $consumer->consume(30 * 1000);
+    $message = $consumer->consume(10 * 1000);
     switch ($message->err) {
         case RD_KAFKA_RESP_ERR_NO_ERROR:
-            var_dump($message);
+            echo "[Consumer] receives the message：" . var_export($message, true) . "\n";
             break;
         case RD_KAFKA_RESP_ERR__PARTITION_EOF:
             echo "No more messages; will wait for more\n";
