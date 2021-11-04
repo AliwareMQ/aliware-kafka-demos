d269a6b7d33ddf16a02910276206cad2463cc310
lj01167294 <lj01167294@alibaba-inc.com>
2021-11-04 15:09:03 +0800

    add dotnet demo vpc and vpc-ssl

---
+++ kafka-dotnet-demo/vpc-ssl/producer/Producer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Confluent.Kafka;
+
+class Producer
+{
+    public static void Main(string[] args)
+    {
+        var conf = new ProducerConfig {
+            BootstrapServers = "XXX,XXX,XXX",
+            SslCaLocation = "XXX/ca-cert.pem",
+            SaslMechanism = SaslMechanism.Plain,
+            SecurityProtocol = SecurityProtocol.SaslSsl,
+            SaslUsername = "XXX",
+            SaslPassword = "XXX",
+        };
+
+        Action<DeliveryReport<Null, string>> handler = r =>
+            Console.WriteLine(!r.Error.IsError
+                ? $"Delivered message to {r.TopicPartitionOffset}"
+                : $"Delivery Error: {r.Error.Reason}");
+
+        string topic ="XXX";
+
+        using (var p = new ProducerBuilder<Null, string>(conf).Build())
+        {
+            for (int i=0; i<100; ++i)
+            {
+                p.Produce(topic, new Message<Null, string> { Value = i.ToString() }, handler);
+                p.Flush(TimeSpan.FromSeconds(10));
+            }
+            //p.Flush(TimeSpan.FromSeconds(10));
+        }
+    }
+}
---
+++ kafka-dotnet-demo/vpc/consumer/Consumer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+class Consumer
+{
+    public static void Main(string[] args)
+    {
+        var conf = new ConsumerConfig {
+            GroupId = "XXX",
+            BootstrapServers = "XXX,XXX,XXX",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        string topic = "XXX";
+
+        using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+        {
+            c.Subscribe(topic);
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var cr = c.Consume(cts.Token);
+                        Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                c.Close();
+            }
+        }
+    }
+}
