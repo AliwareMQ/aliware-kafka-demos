ef87a3efff910e6817f50402185c4e9c4c1577bc
ik8svip <ik8s.vip@gmail.com>
2023-12-09 02:21:46 +0800

    Add Dockerfile, update setting for  kafka-confluent-python-demo

--- kafka-confluent-python-demo/vpc/setting.py
+++ kafka-confluent-python-demo/vpc/setting.py
@@ -1,5 +1,11 @@
+import os
+
+bootstrap_servers = os.environ.get('ALI_KAFKA_BOOTSTRAP_SERVERS', 'kafka-1:9092,kafka-2:9092.kafka-3:9092').split(',')
+topic_name        = os.environ.get('ALI_KAFKA_TOPIC_NAME', 'YOUR_TOPIC_NAME')
+consumer_id       = os.environ.get('ALI_KAFKA_CONSUMER_ID', 'YOUR_GROUP_ID')
+
 kafka_setting = {
-    'bootstrap_servers': 'xxx:xx,xxx:xx',
-    'topic_name': 'xxx',
-    'group_name': 'xxx'
+    'bootstrap_servers': bootstrap_servers,
+    'topic_name': topic_name,
+    'group_name': consumer_id,
 }
